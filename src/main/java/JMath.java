d0c52d96e081b7ec101475903ebf6c3b3a672345
JonathanK <cavaj.0815@gmail.com>
2020-06-15 19:40:38 -0500

    Initial commit

---
+++ src/main/java/JMath.java
@@ -0,0 +1,5 @@
+public class JMath {
+    public static int addInt(int int1, int int2) {
+        return int1 + int2;
+    }
+}
---
+++ src/test/java/JMathTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JMathTest {
+    @Test
+    public void TestAddInt(){
+        assertEquals(2,JMath.addInt(1,1));
+    }
+}
